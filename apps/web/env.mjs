16d80d13f004682a30878db8f44b728ff5e8b542
Tomás Cichero <martoxdlol@gmail.com>
2024-01-30 16:36:12 -0300

    add all neccessary env vars

--- apps/web/env.mjs
+++ apps/web/env.mjs
@@ -3,8 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
     server: {
+        NEXTAUTH_SECRET: z.string().min(1),
         GOOGLE_CLIENT_ID: z.string().min(1),
         GOOGLE_CLIENT_SECRET: z.string().min(1),
+        DATABASE_PRISMA_URL: z.string().min(1),
+        DATABASE_URL_NON_POOLING: z.string().min(1),
     },
     runtimeEnv: {
         ...process.env,
