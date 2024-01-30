8104fe55bf22c87469c1c9d194595d00c1071ee4
Tomás Cichero <martoxdlol@gmail.com>
2024-01-30 13:55:22 -0300

    prisma monorepo workaround

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
   transpilePackages: ["@repo/ui"],
 };
